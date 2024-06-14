582d6e2fdb3e61c0681b4335a0dbeb990853bd1a
Abourkia_Chihab <abourkiachaab@gmail.com>
2024-06-14 15:26:03 +0100

    creation de la class user

---
+++ movieSerie/src/main/java/com/movie_app/movieSerie/entities/User.java
@@ -0,0 +1,22 @@
+package com.movie_app.movieSerie.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_user;
+    private String username;
+    private String email;
+    private String password;
+
+}
